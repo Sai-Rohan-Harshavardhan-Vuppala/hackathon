23c810ccdf98d6a7bbcfa9264b76df3d28d3ab27
Kartikeya <pk39@iitbbs.ac.in>
2023-01-07 00:15:52 +0530

    Added wrapper

---
+++ client/src/wrapper/wrapper.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router";
+
+const Wrapper = () => {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+};
+export default Wrapper;
